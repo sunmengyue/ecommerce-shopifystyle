04f6c59a29cdf10b09a9a2809faa8a63912c9b7d
bardrabbit <bardrabbit@gmail.com>
2021-08-13 15:20:59 -0400

    add transition effect to header text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import '../App.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const Header = () => {
   const texts = [
@@ -18,12 +21,34 @@ const Header = () => {
     return () => clearInterval(intervalId);
   }, [shuffle]);
 
+  // Go to next/previous text
+  const handleNext = () => {
+    setTextIdx((prev) => (prev === texts.length - 1 ? 0 : prev + 1));
+  };
+
+  const handlePrevious = () => {
+    setTextIdx((prev) => (prev === 0 ? texts.length - 1 : prev - 1));
+  };
+
   return (
     <header className="text-white bg-yellow-800">
       <div className="flex p-1 justify-between text-sm items-center max-w-2xl m-auto">
-        <ChevronLeftIcon className="h-6" />
-        <h5>{texts[textIdx]}</h5>
-        <ChevronRightIcon className="h-6" />
+        <ChevronLeftIcon className="h-6 cursor-pointer" onClick={handleNext} />
+        <SwitchTransition mode="out-in">
+          <CSSTransition
+            key={uuidv4()}
+            addEndListener={(node, done) => {
+              node.addEventListener('transitionend', done, false);
+            }}
+            classNames="transition"
+          >
+            <h5>{texts[textIdx]}</h5>
+          </CSSTransition>
+        </SwitchTransition>
+        <ChevronRightIcon
+          className="h-6 cursor-pointer"
+          onClick={handlePrevious}
+        />
       </div>
     </header>
   );
