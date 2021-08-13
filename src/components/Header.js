f6f77997a96186cff760c3cd1e7c2736fe2b9871
sunmengyue <mengyuesun@gmail.com>
2021-08-13 18:32:06 -0400

    get rid of chevron

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,20 +20,9 @@ const Header = () => {
     let intervalId = setInterval(shuffle, 3000);
     return () => clearInterval(intervalId);
   }, [shuffle]);
-
-  // Go to next/previous text
-  const handleNext = () => {
-    setTextIdx((prev) => (prev === texts.length - 1 ? 0 : prev + 1));
-  };
-
-  const handlePrevious = () => {
-    setTextIdx((prev) => (prev === 0 ? texts.length - 1 : prev - 1));
-  };
-
   return (
-    <header className="text-white bg-yellow-800">
-      <div className="flex p-1 justify-between text-sm items-center max-w-2xl m-auto">
-        <ChevronLeftIcon className="h-6 cursor-pointer" onClick={handleNext} />
+    <header className="text-white bg-yellow-800 text-center">
+      <div className="p-1 text-sm max-w-2xl">
         <SwitchTransition mode="out-in">
           <CSSTransition
             key={uuidv4()}
@@ -45,10 +34,6 @@ const Header = () => {
             <h5>{texts[textIdx]}</h5>
           </CSSTransition>
         </SwitchTransition>
-        <ChevronRightIcon
-          className="h-6 cursor-pointer"
-          onClick={handlePrevious}
-        />
       </div>
     </header>
   );
