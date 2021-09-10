8626b238278d6b2418bfd1bd96a9735581b3c660
sunmengyue <mengyuesun@gmail.com>
2021-09-10 01:42:11 -0400

    modify currency calculation and display

--- src/pages/PlaceOrder.js
+++ src/pages/PlaceOrder.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import CheckOutSteps from '../components/CheckOutSteps';
 
 const PlaceOrder = () => {
   const cart = useSelector((state) => state.cart);
+
+  // Calculate Prices
+  cart.itemsPrice = cart.cartItems.reduce(
+    (acc, cur) => acc + cur.price * cur.qty,
+    0,
+  );
+
+  cart.shippingPrice = cart.itemsPrice > 45 ? 0 : 17;
+  cart.taxPrice = 0.075 * cart.itemsPrice;
+  cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+
+  const placeOrderHandler = () => {
+    console.log('Place Order');
+  };
 
   return (
     <div className="max-w-7xl m-auto mt-10 space-y-7 tracking-widest p-5">
@@ -13,7 +28,7 @@ const PlaceOrder = () => {
       <div className="w-full m-auto ">
         <div className="md:flex md:justify-between md:space-x-10">
           {/* Shipping Info */}
-          <div className="flex-grow flex flex-col mb-8">
+          <div className="flex-grow flex flex-col mb-8 md:w-1/2">
             <div className="border-b">
               <h2 className="h2 py-5">Shipping</h2>
               <p className="py-5">
@@ -45,10 +60,13 @@ const PlaceOrder = () => {
                           width={80}
                         />
                       </div>
-                      <p>{item.title}</p>
+                      <Link to={`/products/${item._id}`}>
+                        <p className="link">{item.title}</p>
+                      </Link>
                     </div>
                     <p className="pt-2">
-                      {item.qty} x ${item.price} = ${item.qty * item.price}
+                      {item.qty} x ${item.price} = $
+                      {(item.qty * item.price).toFixed(2)}
                     </p>
                   </div>
                 ))
@@ -60,19 +78,19 @@ const PlaceOrder = () => {
             <h2 className="h2 p-5 border-b">Order Summary</h2>
             <div className="order_summary_item">
               <p className="">Items</p>
-              <p>$189.97</p>
+              <p>${cart.itemsPrice.toFixed(2)}</p>
             </div>
             <div className="order_summary_item">
               <p className="">Shipping</p>
-              <p>$0</p>
+              <p>${cart.shippingPrice.toFixed(2)}</p>
             </div>
             <div className="order_summary_item">
               <p className="">Tax</p>
-              <p>$28.5</p>
+              <p>${cart.taxPrice.toFixed(2)}</p>
             </div>
             <div className="order_summary_item">
               <p className="">Total</p>
-              <p>$189.97</p>
+              <p>${cart.totalPrice.toFixed(2)}</p>
             </div>
             <button className="bg-black text-white px-9 py-3 uppercase tracking-widest align-center hover:bg-gray-800">
               Place Order
